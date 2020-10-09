4e4b409ad740cf89cfa46e0158f27e7311c6fef0
志宇 <mail@evanlinjin.me>
2020-10-09 21:29:02 +1300

    Various fixes. * readable.UnspentOutput now contains base64 encoding of prog. state

--- src/api/client.go
+++ src/api/client.go
@@ -591,10 +591,10 @@ func (c *Client) Balance(addrs []string) (*BalanceResponse, error) {
 func (c *Client) ProgramState(addrs []string) ([]byte, error) {
 	v := make(url.Values, 1)
 	v.Add("addrs", strings.Join(addrs, ","))
-	endpoint := "/api/v1/programState"
+	endpoint := "/api/v1/programState?" + v.Encode()
 
 	var b []byte
-	if err := c.PostForm(endpoint, strings.NewReader(v.Encode()), &b); err != nil {
+	if err := c.Get(endpoint, &b); err != nil {
 		return nil, err
 	}
 	return b, nil
